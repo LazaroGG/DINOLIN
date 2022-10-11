c237dbb1479b846326cb9d3ee267063f64f3d1ea
Lenuario <lazarorx2@gmail.com>
2022-10-11 14:44:14 -0300

    Update Duc

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,9 +1,10 @@
 import pygame
 
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import DUCKING, RUNNING, JUMPING
 
 X_POS = 80
 Y_POS = 310
+Y_POS_DUCK = 340
 JUMP_VEL = 8.5
 
 class Dinosaur:
@@ -15,6 +16,7 @@ class Dinosaur:
         self.step_index = 0
         self.dino_run = True
         self.dino_jump = False
+        self.dino_duck = False
         self.jump_vel = JUMP_VEL
 
     def update(self, user_input):
@@ -22,6 +24,8 @@ class Dinosaur:
             self.run()
         elif self.dino_jump:
             self.jump()
+        elif self.dino_duck:
+            self.duck()
 
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
@@ -29,6 +33,15 @@ class Dinosaur:
         elif not self.dino_jump:
             self.dino_jump = False
             self.dino_run = True
+
+        if user_input[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_duck = True
+            self.dino_run = False
+            self.dino_jump = False
+        elif not self.dino_duck:
+            self.dino_duck = False
+            self.dino_run = True
+            self.dino_jump = False
 
         if self.step_index >= 10:
             self.step_index = 0
@@ -55,4 +68,8 @@ class Dinosaur:
             self.jump_vel = JUMP_VEL
 
     def duck(self):
-        pass
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = X_POS
+        self.dino_rect.y = Y_POS_DUCK
+        self.step_index += 1
